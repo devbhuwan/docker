126edda91d0a188b801036a32606b2e3026b0f5d
Bhuwan Upadhyay <comp.er.bhuwan@gmail.com>
2017-01-08 19:59:58 +0300

    edit SpagoBI

---
+++ spagobi/all-in-one/spagobi.mk.bat
@@ -0,0 +1,3 @@
+del spagobi-all-in-one.dbox
+docker build -f spagobi.docker -t devbhuwan/spagobi-all-in-one .
+docker save devbhuwan/spagobi-all-in-one -o spagobi-all-in-one.dbox
---
+++ spagobi/all-in-one/spagobi.run.bat
@@ -0,0 +1,2 @@
+docker run -d -p 8080:8080 --name "spagobi_5_2_0" devbhuwan/spagobi-all-in-one
+start "" http://192.168.99.100:8080/SpagoBI
