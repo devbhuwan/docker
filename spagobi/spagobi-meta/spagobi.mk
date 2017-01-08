4f539250ce41dd7580526ecfa3b33fcaed78f944
Bhuwan Upadhyay <comp.er.bhuwan@gmail.com>
2017-01-08 21:53:11 +0300

    edit SpagoBI

--- spagobi/spagobi-meta/spagobi.mk
+++ spagobi/spagobi-meta/spagobi.mk
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm spagobi-meta.dbox
-docker build -f spagobi-tools.docker -t devbhuwan/spagobi-meta .
+docker build -f spagobi.docker -t devbhuwan/spagobi-meta .
 docker save devbhuwan/spagobi-meta -o spagobi-meta.dbox
--- spagobi/spagobi-meta/spagobi.mk.bat
+++ spagobi/spagobi-meta/spagobi.mk.bat
@@ -1,3 +1,3 @@
 del spagobi-meta.dbox
-docker build -f spagobi-tools.docker -t devbhuwan/spagobi-meta .
+docker build -f spagobi.docker -t devbhuwan/spagobi-meta .
 docker save devbhuwan/spagobi-meta -o spagobi-meta.dbox
