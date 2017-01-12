6a9a0accae71b59a1d643489bc0adaa121ea5175
Bhuwan Upadhyay <comp.er.bhuwan@gmail.com>
2017-01-12 15:43:05 +0300

    add dahboard-builder

--- jboss-dashboard-builder/dashboard-builder.run.bat
+++ jboss-dashboard-builder/dashboard-builder.run.bat
@@ -1,4 +1,6 @@
+docker stop dashboard-builder-6-2-0
 docker rm dashboard-builder-6-2-0
 docker run -d -p 8080:8080 --name "dashboard-builder-6-2-0" devbhuwan/dashboard-builder
 timeout /t 10 /nobreak
-docker exec -it dashboard-builder-6-2-0 /bin/sh -c "tail -f /usr/local/spagobi/logs/catalina.out"
+docker exec -it dashboard-builder-6-2-0 bash
+::http://containerip:8080/dashbuilder
